d7d88587925547bd4d2b44f50eaccb4bb2231f44
Jean Pierre Zacarias Martinez <jeanrenzo77@gmail.com>
2021-04-14 22:06:42 -0500

    cambio3

--- src/poo/aplicacion.java
+++ src/poo/aplicacion.java
@@ -4,5 +4,6 @@ public class aplicacion {
     public static void main(String[] args){
         System.out.println("hola");
         System.out.println("holita :3");
+        System.out.println("acabarapido");
     }
 }
